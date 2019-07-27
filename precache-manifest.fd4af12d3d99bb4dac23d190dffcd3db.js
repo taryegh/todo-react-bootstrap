004759730bd8357d688cd9fef34ed031cdef00fe
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-07-27 21:53:51 +0400

    Updates

---
+++ precache-manifest.fd4af12d3d99bb4dac23d190dffcd3db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d5bc4a5903945a54c468b1fdc6b72e",
+    "url": "/todo-react-bootstrap/index.html"
+  },
+  {
+    "revision": "4dca7fe4dcafb85863e5",
+    "url": "/todo-react-bootstrap/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6b5e11094e3dd06e7571",
+    "url": "/todo-react-bootstrap/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4dca7fe4dcafb85863e5",
+    "url": "/todo-react-bootstrap/static/js/2.c3bcc122.chunk.js"
+  },
+  {
+    "revision": "6b5e11094e3dd06e7571",
+    "url": "/todo-react-bootstrap/static/js/main.372b6dba.chunk.js"
+  },
+  {
+    "revision": "dd9759e24ffc21a12f06",
+    "url": "/todo-react-bootstrap/static/js/runtime~main.d99f84c8.js"
+  }
+]);
